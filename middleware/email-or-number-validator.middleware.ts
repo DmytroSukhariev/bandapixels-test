7cc8a5122fb9be06279c6febe457f98e932b9680
Dmytro Sukhariev <dmytro@mainframe>
2020-07-29 11:55:34 +0300

    reges vulnarability fix

--- middleware/email-or-number-validator.middleware.ts
+++ middleware/email-or-number-validator.middleware.ts
@@ -3,13 +3,19 @@ import { body } from 'express-validator';
 import { Id } from '../types';
 
 const validateEmail = (id: Id) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(id).toLowerCase());
+    if (id.length >= 40) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(id).toLowerCase());
+    }
+    return false;
 }
 
 const validatePhoneNumber = (id: Id) => {
-    const re = /^\+?3?8?(0\d{9})$/;
-    return re.test(String(id).toLowerCase());
+    if (id.length >= 14) {
+        const re = /^\+?3?8?(0\d{9})$/;
+        return re.test(String(id).toLowerCase());
+    }
+    return false;
 }
 
 export const emailOrNumberValidator = body('id')
